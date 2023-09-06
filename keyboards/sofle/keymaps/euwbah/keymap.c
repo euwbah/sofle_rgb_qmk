c813a50bcd8cb9d99f692d3129252fa64426abf3
euwbah <euwbah@gmail.com>
2023-09-07 02:44:44 +0800

    clean up keymap formatting

--- keyboards/sofle/keymaps/euwbah/keymap.c
+++ keyboards/sofle/keymaps/euwbah/keymap.c
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,  KC_D_MUTE,KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE, KC_D_MUTE, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-                 KC_LALT, KC_LGUI, KC_LOWER, KC_LSFT,  KC_SPC,    KC_NAV,  KC_SPC,  KC_RAISE, KC_LPRN,  KC_RPRN
+                 KC_LALT, KC_LGUI, KC_LOWER, KC_LSFT, KC_SPC,     KC_NAV, KC_SPC, KC_RAISE,KC_LPRN, KC_RPRN
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
 ),
 
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   KC_F13,   KC_F14, KC_F15,  KC_F16,   KC_F17,  KC_F18,                   KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-  _______,C(S(KC_ESC)),C(A(KC_DEL)),KC_PSCR,KC_WH_U,KC_WH_L,                KC_BRIU, KC_VOLU, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN4,
+  _______,C(S(KC_ESC)),C(A(KC_DEL)),KC_PSCR,KC_WH_U,KC_WH_L,              KC_BRIU, KC_VOLU, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN4,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
   _______,  KC_NO,  KC_NO,   KC_NO,    KC_WH_D, KC_WH_R,_______, _______, KC_BRID, KC_VOLD, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN5,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
@@ -160,13 +160,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // KC_LANG2 switches to half-width alphanumeric. Also hanja conversion key.
 [_RAISE] = LAYOUT(
   //,------------------------------------------------.                    ,---------------------------------------------------.
-  KC_GRV,    KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_F7,  KC_F8  , KC_F9,  KC_F10 , KC_F11 , KC_F12,
+  KC_GRV,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10 , KC_F11 , KC_F12,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-  KC_TILD,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                      KC_6,   KC_7,    KC_8,    KC_9,     KC_0,    KC_QUES,
+  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUES,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-  _______, KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR,  KC_PERC,                 KC_CIRC,   KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS,
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-  _______, KC_QUES, _______, KC_LPRN, KC_RPRN,  KC_LT,_______,    _______,KC_GT,   KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, KC_PIPE,
+  _______, KC_QUES, _______, KC_LPRN, KC_RPRN, KC_LT, _______,   _______, KC_GT,   KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, KC_PIPE,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
                  _______, _______, _______, _______, _______,     KC_LNG1, KC_LNG2, _______, KC_EQL,  KC_AMPR
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
